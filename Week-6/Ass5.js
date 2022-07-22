3d58d262efb7048c59d18013ad48df7ec6cb9231
Milind Chaudhari <milind220397@gmail.com>
2022-07-23 03:41:57 +0530

    'Week6_assignment6_changes'

--- Week-6/Ass5.js
+++ Week-6/Ass5.js
@@ -1,26 +1,24 @@
-function diff(a,b){
+function diff(a, b) {
 
-    for(let i = 0 ;i<a.length ;i++)
-        {
-            for(let j = i + 1 ;j<a.length ;j++)
-            { 
-                // difference
-                let dif = a[j] - a[i];
-                if (dif<0)
-                  dif = dif * -1;
+    for (let i = 0; i < a.length; i++) {
+        for (let j = i + 1; j < a.length; j++) {
+            // difference
+            let dif = a[j] - a[i];
+            if (dif < 0)
+                dif = dif * -1;
 
-                if ( dif == b )
-                {
-                   return 1
-                }
+            if (dif == b) {
+                return 1
             }
         }
-        
-    return 0;
-    
     }
-    
-    
-    var a = [ 5,10,3,2,50,80 ]
-    let b = 11
-    console.log(diff(a,b));
+
+
+    return 0;
+
+}
+
+
+var a = [5, 10, 3, 2, 50, 80]
+let b = 4
+console.log(diff(a, b));
