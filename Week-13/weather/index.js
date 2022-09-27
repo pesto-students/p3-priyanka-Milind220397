d94b05b1b6b7ac6a6d37e2f45b8ae0f94978c8cd
Milind Chaudhari <milind220397@gmail.com>
2022-09-28 01:35:56 +0530

    Week13_Changes

---
+++ Week-13/weather/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const cors=require('cors')
+const routes=require('./routes/routes')
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(cors())
+app.use(express.json())
+app.use('/',routes)
+app.listen(PORT, () => {
+    console.log(process.env.API_KEY);
+  console.log(`server running at 127.0.0.1:${PORT}`);
+});
